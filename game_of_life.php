d218deee6ff10af4ff285981a15f9c34508fecb1
Tom Yates <tomy8s@hotmail.com>
2015-12-17 23:14:18 +0500

    Game of life with checkboxes and random pattern generator

--- game_of_life.php
+++ game_of_life.php
@@ -10,7 +10,12 @@ if (isset($_POST['gen'])) {
     $gen = 0;
 }
 $gen ++;
-
+if (isset($_GET['random']) && (intval($_GET['live']) > 0) && (intval($_GET['live']) <= ($rows * $cols))) {
+    $live = intval($_GET['live']);
+} else if (isset($_GET['random'])) {
+    $live = rand(1, $rows * $cols);
+}
+//Check grid parameters
 if (($rows < 1) || ($cols < 1)) {
     ?>
     <P>Please enter an interger above 0.<br>
@@ -20,30 +25,65 @@ if (($rows < 1) || ($cols < 1)) {
     <p>Your grid has <?php echo $rows;?> rows and <?php echo $cols;?> columns.</p><p style="font-family: 'Lucidia-Console', monospace" align="center">
     <form action="game_of_life.php?rows=<?php echo $rows;?>&columns=<?php echo $cols;?>" method="post">
     <?php
-    while ($rowno <= $rows) {
-        $colno = 1;
-        while ($colno <= $cols) {
-            echo '<input type="checkbox" name="'.$rowno.','.$colno.'"';
-//Start of live/dead test
-            $neicells = array(
-                isset($_POST[($rowno-1).','.($colno-1)]), isset($_POST[($rowno-1).','.($colno)]), isset($_POST[($rowno-1).','.($colno+1)]),
-                isset($_POST[($rowno).','.($colno-1)]), isset($_POST[($rowno).','.($colno+1)]),
-                isset($_POST[($rowno+1).','.($colno-1)]), isset($_POST[($rowno+1).','.($colno)]), isset($_POST[($rowno+1).','.($colno+1)]));
-            $neino = 0;
-            foreach ($neicells as $nei) {
-                if ($nei == true) {$neino ++;}
+    echo $gen.' gen<br>';
+//Random cell generator
+    if ($gen == 1 && isset($_GET['random'])) {
+        $randomnos = array_fill(0, $live, NULL);
+        for ($i = 0; $i < $live; $i ++) {
+            $newrand = rand(1, $cols * $rows);
+            while (in_array($newrand, $randomnos)) {
+                $newrand = rand(1, $cols * $rows);
             }
-            //if (isset($_POST[$rowno.','.$colno])&&($_POST[$rowno.','.$colno])){
-            if ((isset($_POST[$rowno.','.$colno])&& ($neino == 2 || $neino == 3)) || (!isset($_POST[$rowno.','.$colno])&& $neino == 3)) {
-                echo ' checked>';
-            } else {
-                echo '>';
+            $randomnos[$i] = $newrand;
+        }
+        //$randkey = 0;
+        //print_r($randomnos);
+        //echo '<br>';
+        //foreach ($randomnos as $randkey => $randval) {
+        //    echo 'Value = '.$randval.' row = '.(intval($randval/$cols)+1).' col = '.($randval%$cols).'<br>';
+        //}
+        //echo $live.' live<br>';
+        while ($rowno <= $rows) {
+            $colno = 1;
+            while ($colno <= $cols) {
+                echo '<input type="checkbox" name="'.$rowno.','.$colno.'"';
+                if (in_array(((($rowno-1)*$cols)+$colno), $randomnos)){
+                    echo ' checked>';
+                } else {
+                    echo '>';
+                }
+                $colno ++;
             }
-//End of live/dead test
-            $colno ++;
-        }        
-        echo '<br>';
-        $rowno ++;
+            echo '<br>';
+            $rowno ++;
+        }
+    } else {
+        while ($rowno <= $rows) {
+            $colno = 1;
+            while ($colno <= $cols) {
+                echo '<input type="checkbox" name="'.$rowno.','.$colno.'"';
+    //Start of live/dead test
+                $neicells = array(
+                    isset($_POST[($rowno-1).','.($colno-1)]), isset($_POST[($rowno-1).','.($colno)]), isset($_POST[($rowno-1).','.($colno+1)]),
+                    isset($_POST[($rowno).','.($colno-1)]), isset($_POST[($rowno).','.($colno+1)]),
+                    isset($_POST[($rowno+1).','.($colno-1)]), isset($_POST[($rowno+1).','.($colno)]), isset($_POST[($rowno+1).','.($colno+1)]));
+                $neino = 0;
+                foreach ($neicells as $nei) {
+                    if ($nei == true) {
+                        $neino ++;
+                    }
+                }
+                if (((isset($_POST[$rowno.','.$colno]) && ($neino == 2 || $neino == 3)) || (!isset($_POST[$rowno.','.$colno])&& $neino == 3))) {
+                    echo ' checked>';
+                } else {
+                    echo '>';
+                }
+    //End of live/dead test
+                $colno ++;
+            }
+            echo '<br>';
+            $rowno ++;
+        }
     }
     ?>
     <p>
--- q1.php
+++ q1.php
@@ -1,7 +1,6 @@
 <?php
     $neicells = array(
-        $_POST[$rowno-1 .','.$colno-1],
-        $_POST[$rowno-1 .','.$colno], $_POST[$rowno-1 .','.$colno+1],
+        $_POST[$rowno-1 .','.$colno-1], $_POST[$rowno-1 .','.$colno], $_POST[$rowno-1 .','.$colno+1],
         $_POST[$rowno.','.$colno-1], $_POST[$rowno.','.$colno+1],
         $_POST[$rowno+1 .','.$colno-1], $_POST[$rowno+1 .','.$colno], $_POST[$rowno+1 .','.$colno+1]);
     echo $neicells;
